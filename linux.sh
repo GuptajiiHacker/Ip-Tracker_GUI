0d9ce891477c2cfa312f121972648ed37743c1d1
HackerGuptaji <155041576+GuptajiiHacker@users.noreply.github.com>
2024-08-17 11:44:01 +0530

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -9,9 +9,9 @@ mkdir /$HOME/.Ip-Tracker_GUI
 mv .index.php /$HOME/.Ip-Tracker_GUI/index.php
 mv .style.css /$HOME/.Ip-Tracker_GUI/style.css
 mv .logo.png /$HOME/.Ip-Tracker_GUI/logo.png
-echo "php -S localhost:9999 -t /$HOME/.Ip-Tracker_GUI" >> ip-track
-mv tracex /usr/local/bin
-chmod +x /usr/local/bin/ip-track
+echo "php -S localhost:9999 -t /$HOME/.Ip-Tracker_GUI" >> ip-track-vip
+mv ip-track-vip/usr/local/bin
+chmod +x /usr/local/bin/ip-track-vip
 figlet DONE
 echo 'Installation is completed now you can launch TraceX-GUI by just typing ip-track in your terminal'
 rm -rfv ../Ip-Tracker_GUI
